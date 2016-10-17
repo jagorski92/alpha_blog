b6ce683eb5da116b986663fc039262956a362f44
Jacob Heydorn Gorski <heydorngorski@gmail.com>
2016-10-17 11:41:16 +0000

    signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 root 'pages#home'
 
 get 'pages/about', to: 'pages#about'
-
     resources :articles 
- 
+
+get 'signup', to: 'users#new'
+    resources :users, except: [:new]
 end
